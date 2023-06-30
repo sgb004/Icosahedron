f37e021abf72fb1374086b4ed0ff6b06f43962bb
sgb004 <sgb004@gmail.com>
2023-06-30 15:29:55 -0600

    Ahora se pueden usar las flechas de arriba, abajo, izquierda, derecha, re pag, av pag, r y Home para controlar la rotación del poligono.

--- src/components/polygon/polygon.jsx
+++ src/components/polygon/polygon.jsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useEffect, useRef } from 'react';
 import Face from '../face/face';
 import maxSize from './max-size';
 import './polygon.css';
@@ -8,9 +9,85 @@ const Polygon = ({ faces }) => {
 	console.log(faces);
 
 	const size = maxSize(faces);
+	const polygonRef = useRef();
+
+	useEffect(() => {
+		let rotationX = parseInt(localStorage.getItem('rotationX'));
+		let rotationY = parseInt(localStorage.getItem('rotationY'));
+		let rotationZ = parseInt(localStorage.getItem('rotationZ'));
+
+		if (isNaN(rotationX)) {
+			rotationX = 0;
+		}
+
+		if (isNaN(rotationY)) {
+			rotationY = 0;
+		}
+
+		if (isNaN(rotationZ)) {
+			rotationZ = 0;
+		}
+
+		polygonRef.current.style.setProperty('--rotationAxisX', `${rotationX}deg`);
+		polygonRef.current.style.setProperty('--rotationAxisY', `${rotationY}deg`);
+		polygonRef.current.style.setProperty('--rotationAxisZ', `${rotationZ}deg`);
+
+		const rotationKeyDown = (event) => {
+			if (event.key === 'F5') return;
+
+			event.preventDefault();
+
+			switch (event.key) {
+				case 'ArrowUp':
+					polygonRef.current.style.setProperty('--rotationAxisX', `${rotationX++}deg`);
+					break;
+				case 'ArrowDown':
+					polygonRef.current.style.setProperty('--rotationAxisX', `${rotationX--}deg`);
+					break;
+				case 'ArrowLeft':
+					polygonRef.current.style.setProperty('--rotationAxisZ', `${rotationZ++}deg`);
+					break;
+				case 'ArrowRight':
+					polygonRef.current.style.setProperty('--rotationAxisZ', `${rotationZ--}deg`);
+					break;
+				case 'PageUp':
+					polygonRef.current.style.setProperty('--rotationAxisY', `${rotationY++}deg`);
+					break;
+				case 'PageDown':
+					polygonRef.current.style.setProperty('--rotationAxisY', `${rotationY--}deg`);
+					break;
+				case 'Home':
+				case 'r':
+				case 'R':
+					rotationX = 0;
+					rotationY = 0;
+					rotationZ = 0;
+					polygonRef.current.style.setProperty('--rotationAxisX', `${rotationX}deg`);
+					polygonRef.current.style.setProperty('--rotationAxisY', `${rotationY}deg`);
+					polygonRef.current.style.setProperty('--rotationAxisZ', `${rotationZ}deg`);
+					break;
+				default:
+					break;
+			}
+
+			localStorage.setItem('rotationX', rotationX);
+			localStorage.setItem('rotationY', rotationY);
+			localStorage.setItem('rotationZ', rotationZ);
+		};
+
+		document.addEventListener('keydown', rotationKeyDown);
+
+		return () => {
+			document.removeEventListener('keydown', rotationKeyDown);
+		};
+	}, []);
 
 	return (
-		<div className="polygon" style={{ width: size.width, height: size.height }}>
+		<div
+			ref={polygonRef}
+			className="polygon"
+			style={{ width: size.width, height: size.height }}
+		>
 			{faces.map((face, index) => {
 				console.log(face.rotation);
 
